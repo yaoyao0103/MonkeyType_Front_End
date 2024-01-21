65a2c0383c48a6c603080763c8021aa51acda9c4
yaoyao0103 <elites96105@gmail.com>
2024-01-22 03:40:03 +0800

    feat: get and fresh words completed

--- src/components/TypeBlock/TypeBlock.tsx
+++ src/components/TypeBlock/TypeBlock.tsx
@@ -1,7 +1,7 @@
 import React, { useEffect, useState, useRef } from 'react'
 import './TypeBlock.css'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faEarthAmericas } from '@fortawesome/free-solid-svg-icons'
+import { faEarthAmericas, faRotateRight } from '@fortawesome/free-solid-svg-icons'
 import Popup from '../Popup/Popup'
 import axios from 'axios'
 
@@ -11,12 +11,16 @@ const TypeBlock = (props: Props) => {
     const buttonRef = useRef<HTMLDivElement>(null);
 
     const [words, setWords] = useState<string[]>([]);
+    const [sequenceWords, setSequenceWords] = useState<string>("");
     const [language, setLanguage] = useState<string>("english");
     const [allLanguages, setAllLanguages] = useState<string[]>([]);
     const [isPopup, setIsPopup] = useState<boolean>(false);
+    const [show, setShow] = useState<boolean>(false);
+
     useEffect( () => {
         const list = require('../../static/_list.json');
         setAllLanguages(list);
+        setShow(true);
     },[])
     useEffect(() => {
         if(allLanguages.length !== 0) console.log(allLanguages)
@@ -30,18 +34,55 @@ const TypeBlock = (props: Props) => {
         fetchLanguage();
     }, [language])
 
+    useEffect(() => {
+        setSequenceWords(words.join(" "));
+    }, [words])
+
+    // useEffect(() => {
+    //     if(!show){
+    //         shuffleArray();
+    //         console.log(show);
+    //         setShow(true);
+    //     }
+    // }, [show])
+
     const handleOnSelectLanguage = (selectedLanguage: string) => {
         setLanguage(selectedLanguage);
     }
 
+    const shuffleArray = () => {
+        const new_words: string[] = [...words];
+        for (let i = new_words.length - 1; i > 0; i--) {
+          const j = Math.floor(Math.random() * (i + 1));
+          // Swap array[i] and array[j]
+          [new_words[i], new_words[j]] = [new_words[j], new_words[i]];
+        }
+        setWords(new_words);
+      }
+
+
+    const restart = () => {
+        setShow(false);
+        setTimeout(() => {
+            shuffleArray();
+            setShow(true);
+        }, 100);
+    }
+
+
     return (
         <div className='typeBlock__layout'>
-            <div className='typeBlock__container'>
+            <div className={`typeBlock__container ${show? "":"hidden"}`}>
                 <div className='typeBlock__language' onMouseUp={() => setIsPopup(true)} ref={buttonRef}>
                     <div><FontAwesomeIcon icon={faEarthAmericas} /></div>
                     <div className='text'>{language}</div>
                 </div>
-                <div className='typeBlock__type'></div>
+                <div className='typeBlock__type'>
+                    <div className='typeBlock__words'>{sequenceWords}</div>
+                </div>
+                <div className='typeBlock__restart' onClick={restart}>
+                    <div><FontAwesomeIcon icon={faRotateRight} /></div>
+                </div>
             </div>
             {isPopup&& <Popup isPopup={isPopup} allLanguages={allLanguages} onClose={() => setIsPopup(false)} onSelect={handleOnSelectLanguage}/>}
         </div>
